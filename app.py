7e27cf5b6e48fb8c0fb9bf848f3d8c96581bb99b
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-02-02 13:05:14 +0000

    Added decorators, config loading and template filters

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 import json
+import toml
+import datetime
 
 from handlers.views import weeknotes_index_handler, weeknotes_post_handler, weeknotes_tags_handler, weeknotes_tag_handler, weeknotes_year_handler, weeknotes_month_handler
 from handlers.cli import build_index_handler
+
+from functions.decorators import templated
 
 
 def create_app():
@@ -20,6 +24,8 @@ def create_app():
     """
     app = Flask(__name__)
 
+    app.config.from_file('config.toml', toml.load)
+
     # removing whitespace from templated returns    
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
@@ -29,6 +35,15 @@ def create_app():
 
 
 app = create_app()
+
+
+
+
+
+def load_from_index(index):
+    for key in index:
+        app.data[key] = index[key]
+    pass
 
 
 @app.before_first_request
@@ -41,14 +56,15 @@ def load_data():
     with open('content/index.json') as index_file:
         index = json.load(index_file)
 
-    for key in index:
-        app.data[key] = index[key]
+    load_from_index(index)
     pass
 
 
+# Web handlers
 
 @app.route('/weeknotes/')
 @app.route('/weeknotes')
+@templated('index')
 def index_view():
     return weeknotes_index_handler()
 
@@ -61,34 +77,81 @@ def tags_view():
 
 @app.route('/weeknotes/tags/<string:tag_slug>/')
 @app.route('/weeknotes/tags/<string:tag_slug>')
+@templated('facet')
 def tag_view(tag_slug):
     return weeknotes_tag_handler(tag_slug)
 
 
 @app.route('/weeknotes/years/<string:year>/')
 @app.route('/weeknotes/years/<string:year>')
+@templated('facet')
 def year_view(year):
     return weeknotes_year_handler(year)
 
 
 @app.route('/weeknotes/years/<string:year>/months/<string:month>/')
 @app.route('/weeknotes/years/<string:year>/months/<string:month>/')
+@templated('facet')
 def month_view(year, month):
     return weeknotes_month_handler(year, month)
 
 
 @app.route('/weeknotes/posts/<string:post_slug>/')
 @app.route('/weeknotes/posts/<string:post_slug>')
+@templated('post')
 def post_view(post_slug):
     return weeknotes_post_handler(post_slug)
 
 
 @app.route('/tmp/build_index')
 def build_index_view():
-    return build_index_handler()    
+    if '127.0.0.1' in request.host:
+        print ('BUILDING INDEX VIA URL')
+        index = build_index_handler()
+        load_from_index(index)
+        return index
+    else:
+        return {'error':'Indexing by URL is only supported locally'}
 
+
+#CLI handlers
 
 @app.cli.command('build_index')
 def build_index_command():
-    print ('BUILDING INDEX')
+    print ('BUILDING INDEX VIA CLI')
     return build_index_handler()    
+
+
+# Template filters
+
+@app.template_filter()
+def deslugify(text):
+    """
+    This function takes a slug of a text string and turns it back into a text string (assuming only spaces were in the original string)
+    """
+    return text.replace('_',' ')
+
+
+@app.template_filter()
+def humanize_fulldate(date):
+    ordinal_char = '$'
+    datestring = date.strftime(f'%B %d{ordinal_char} %Y')
+    day_of_month = str(date.strftime('%d'))
+    if day_of_month in ['1','11','21','31']:
+        ordinal = 'st'
+    elif day_of_month in ['2','22']:
+        ordinal = 'nd'
+    elif day_of_month in ['3','23']:
+        ordinal = 'rd'
+    else:
+        ordinal = 'th'
+    datestring = datestring.replace(ordinal_char, ordinal)
+    return datestring
+
+
+@app.template_filter()
+def truncate_list_posts(html):
+    paragraphs = html.split('</p>')
+    truncated_html = '</p>'.join(paragraphs[:1])
+    truncated_html += '</p>'
+    return truncated_html
